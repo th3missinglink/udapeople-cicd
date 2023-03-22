ca6d3de7bf0e572b00c72115c52b93d87bd04731
th3missinglink <anader370@gmail.com>
2023-03-22 13:20:04 -0500

    SmokeTest08

--- backend/set_env.sh
+++ backend/set_env.sh
@@ -4,6 +4,7 @@ export TYPEORM_CONNECTION=postgres
 export TYPEORM_MIGRATIONS_DIR=./migrations
 export TYPEORM_ENTITIES=./modules/domain/**/*.entity{.ts,.js}
 export TYPEORM_MIGRATIONS=./migrations/*{.ts,.js}
+
 export TYPEORM_HOST=localhost
 export TYPEORM_PORT=5532
 export TYPEORM_USERNAME=postgres
